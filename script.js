889b2c65653e1268085932826e71b1558f445c50
rogueassassin11 <maecagadas2011@gmail.com>
2025-07-13 22:28:29 +0800

    Until Going Back to First DOM Manipulation

---
+++ script.js
@@ -0,0 +1,33 @@
+// TOGGLING FORM
+
+const btn = document.querySelector(".btn-open");
+const form = document.querySelector(".fact-form");
+
+btn.addEventListener("click", function () {
+  if (form.classList.contains("hidden")) {
+    form.classList.remove("hidden");
+    btn.textContent = "Close";
+  } else {
+    form.classList.add("hidden");
+    btn.textContent = "Share a fact";
+  }
+});
+
+let votesInteresting = 23;
+let votesMindblowing = 5;
+const text = "Joel is cool.";
+
+const CATEGORIES = [
+  { name: "technology", color: "#3b82f6" },
+  { name: "science", color: "#16a34a" },
+  { name: "finance", color: "#ef4444" },
+  { name: "society", color: "#eab308" },
+  { name: "entertainment", color: "#db2777" },
+  { name: "health", color: "#14b8a6" },
+  { name: "history", color: "#f97316" },
+  { name: "news", color: "#8b5cf6" },
+];
+
+const allCategories = CATEGORIES.map((el) => el.name);
+
+console.log(allCategories);
